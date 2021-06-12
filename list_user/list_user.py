8d4bbe72a2c8ca0bb399da2b77e96ac7f2f25f24
kritsanee1004 <84364894+kritsanee1004@users.noreply.github.com>
2021-06-12 07:43:03 -0700

    Update list_user.py

--- list_user/list_user.py
+++ list_user/list_user.py
@@ -1,18 +1,45 @@
-from flask import Flask, jsonify
-from flask import request
+from flask import Flask, request, jsonify, make_response, json, send_from_directory, redirect, url_for
 import pymongo
 import json
 import sys
-import database1
+#import database1
+import pika
+import logging
+import warnings
+import bcrypt
 
-# database part
+# packages for swagger
+from flasgger import Swagger
+from flasgger import swag_from
+
+# setup flask app
 app = Flask(__name__)
-db_add = "mongodb2://192.168.35.128:27018/"
 
-@app.route('/list_user', methods=['GET'])
-def list_user():
-    data1 = database1.main(db_add)
-    return jsonify(data1)
+# setup swagger online document
+swagger = Swagger(app)
+
+# setup logger
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+@app.route('/list_user',methods=['GET'])
+@swag_from('apidocs/api_list_user.yml')
+def listuser():
+    myclient = pymongo.MongoClient("mongodb://192.168.35.128:27018/")
+    #db_add = "mongodb://192.168.0.45:27018/"
+    mydb = myclient["mydatabase"]
+    users = mydb["key"]
+
+    db_list = []
+    for x in users.find({},{"Password" : 0, "Own" : 0 , "Debt" : 0 }):
+        data = str(x).replace("ObjectId(","")
+        data = data.replace(")","")
+        data = data.replace("'",'"')
+        data = data.replace('"data": "{','')
+        data = data.replace('}"','')
+        db_list.append(json.loads(data))
+
+    return json.dumps(db_list)
 
 @app.route('/')
 def index():
